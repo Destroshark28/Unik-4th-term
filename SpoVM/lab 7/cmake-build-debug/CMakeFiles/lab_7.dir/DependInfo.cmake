df014c06fe622b21c4217dd4de55e7e268110e4b
Ilya Pechuro <ilyapechuro991@gmail.com>
2019-05-12 16:13:25 +0300

    Added apk and kp labs

---
+++ SpoVM/lab 7/cmake-build-debug/CMakeFiles/lab_7.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ilyshka/Desktop/Unik/SpoVM/lab 7/filesystem/impl/disk.cpp" "/home/ilyshka/Desktop/Unik/SpoVM/lab 7/cmake-build-debug/CMakeFiles/lab_7.dir/filesystem/impl/disk.cpp.o"
+  "/home/ilyshka/Desktop/Unik/SpoVM/lab 7/filesystem/impl/fs.cpp" "/home/ilyshka/Desktop/Unik/SpoVM/lab 7/cmake-build-debug/CMakeFiles/lab_7.dir/filesystem/impl/fs.cpp.o"
+  "/home/ilyshka/Desktop/Unik/SpoVM/lab 7/main.cpp" "/home/ilyshka/Desktop/Unik/SpoVM/lab 7/cmake-build-debug/CMakeFiles/lab_7.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SpoVM/lab 7/cmake-build-debug/CMakeFiles/lab_7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_7.dir/filesystem/impl/disk.cpp.o"
+  "CMakeFiles/lab_7.dir/filesystem/impl/fs.cpp.o"
+  "CMakeFiles/lab_7.dir/main.cpp.o"
+  "lab_7.pdb"
+  "lab_7"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
